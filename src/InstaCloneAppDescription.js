120c2e3a7d6d82243226bf4f2083f9fcee4924da
Ansuman09 <ansumansamal1@gmail.com>
2024-10-22 00:19:17 +0530

    commit

--- src/InstaCloneAppDescription.js
+++ src/InstaCloneAppDescription.js
@@ -1,9 +1,30 @@
 import React from "react";
+import NavBar from "./NavBar";
 
 const InstaCloneAppDescription=()=>{
     return(
         <div>
-            "This is a new page"
+            <NavBar />
+            <div className="page-navigation-container">
+                <div className="path-choice-container">
+                    <p><b>App Architecture</b></p>
+                </div>
+                <div className="path-choice-container">
+                    <p><b>Database:MYSQL</b></p>
+                </div>
+                <div className="path-choice-container">
+                    <p><b>Database Connect</b></p>
+                </div>
+                <div className="path-choice-container">
+                    <p><b>Spring Authentication</b></p>
+                </div>
+                <div className="path-choice-container">
+                    <p><b>Spring API </b></p>
+                </div>
+                <div className="path-choice-container">
+                    <p><b>Deploying</b></p>
+                </div>
+            </div>
         </div>
     )
 }
